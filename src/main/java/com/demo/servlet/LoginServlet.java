09f3e2b2ff0892ad82e8a9c55806157db62d47a8
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-04 22:39:08 +0530

    HTTP Status 405 – Method Not Allowed

--- src/main/java/com/demo/servlet/LoginServlet.java
+++ src/main/java/com/demo/servlet/LoginServlet.java
@@ -1,7 +1,14 @@
 package com.demo.servlet;
 
-import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/logIn")
 public class LoginServlet extends HttpServlet{
 
 	/**
@@ -9,5 +16,9 @@ public class LoginServlet extends HttpServlet{
 	 * @Date 04-01-2024
 	 */
 	private static final long serialVersionUID = 2309124040007562096L;
-
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.service(req, resp);
+	}
 }
