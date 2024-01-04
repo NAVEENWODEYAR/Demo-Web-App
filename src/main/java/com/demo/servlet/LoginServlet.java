dfaff3e00bfe72d1c665a4d8517aace1ba9753f5
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-04 22:13:59 +0530

    LoginServlet added

---
+++ src/main/java/com/demo/servlet/LoginServlet.java
@@ -0,0 +1,13 @@
+package com.demo.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class LoginServlet extends HttpServlet{
+
+	/**
+	 * @author Naveen Wodeyar
+	 * @Date 04-01-2024
+	 */
+	private static final long serialVersionUID = 2309124040007562096L;
+
+}
