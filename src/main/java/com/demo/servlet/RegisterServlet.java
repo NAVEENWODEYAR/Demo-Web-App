36f3b768955d4e9680357331d841ffb297c525a2
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-05 08:57:46 +0530

    logIn

---
+++ src/main/java/com/demo/servlet/RegisterServlet.java
@@ -0,0 +1,20 @@
+package com.demo.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Naveen Wodeyar
+ */
+@WebServlet("register")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+	}
+}
---
+++ src/main/webapp/register.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1 style="color: blue;font-size: larger;">Registration Page</h1>
+	<font>
+		Name:<input type="text" name="name" placeholder="Name"/><br><br>
+		Mail:<input type="email" name="mail" placeholder="E-Mail"/><br><br>
+	</font>
+</body>
+</html>
