1d208fd2471c57ca105d765868ae15c7e2989767
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-01 22:52:52 +0530

    login.jsp mapped to loginServlet

--- 28-01-24/LoginPageUsingJSP/src/main/java/com/java/servlet/LoginServlet.java
+++ 28-01-24/LoginPageUsingJSP/src/main/java/com/java/servlet/LoginServlet.java
@@ -8,32 +8,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginServlet
+ * @author Naveen K Wodeyar
+ * @Date 01-02-2024
  */
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
