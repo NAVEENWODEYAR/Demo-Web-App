31cefc6cf07b00a7b07cc37e6fba0cd495754221
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-22 23:23:29 +0530

    req.getParameter()

--- src/main/java/com/demo/servlet/RegisterServlet.java
+++ src/main/java/com/demo/servlet/RegisterServlet.java
@@ -16,6 +16,8 @@ public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	    String name = req.getParameter("name");
+	    String password = req.getParameter("password");
 	}
 }
