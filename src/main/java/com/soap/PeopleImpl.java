e5b850a27c858347f53cba542d54ed767b4c899a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-09 00:14:01 +0530

    The Tomcat v9.0 Server server does not support the service project DemoWebApp.

---
+++ src/main/java/com/soap/PeopleImpl.java
@@ -0,0 +1,22 @@
+package com.soap;
+
+public class PeopleImpl implements People {
+    private String name = null;
+    private int age = 0;
+    
+    @Override
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    @Override
+    public String getName() {
+	return name;
+    }
+
+    @Override
+    public int getAge() {
+	return age;
+    }
+
+}
